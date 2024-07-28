6be73154bb62df65f95908bb04436305b7744cf6
realitysynthesizer <157746887+realitysynthesizer@users.noreply.github.com>
2024-07-29 05:28:40 +0530

    added controls information

--- Assets/scripts/WelcomeScreen.cs
+++ Assets/scripts/WelcomeScreen.cs
@@ -3,23 +3,29 @@ using UnityEngine.SceneManagement;
 
 public class WelcomeScreen : MonoBehaviour
 {
+    public GameObject infopanel;
     public void StartGame()
     {
         // Load the main game scene
         SceneManager.LoadScene("main"); // Replace with your main game scene name
     }
 
-    public void OpenOptions()
-    {
-        // Open the options menu
-        // This could involve enabling/disabling UI panels
-        Debug.Log("Options button clicked");
-    }
+    
 
     public void QuitGame()
     {
         // Quit the application
         Application.Quit();
         Debug.Log("Quit button clicked");
+    }
+
+    public void showinfo()
+    {
+        infopanel.SetActive(true);
+    }
+
+    public void hideinfo()
+    {
+        infopanel.SetActive(false);
     }
 }
